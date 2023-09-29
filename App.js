e60da0b1827228481be6d4f4838ffd232f58a7c8
Kauan <kauanmauriciodasilvacruz@gmail.com>
2023-09-29 18:58:02 -0300

    Seu pauNaLomba

--- App.js
+++ App.js
@@ -1,83 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, Alert } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, Modal } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import * as Location from 'expo-location';
+import DriverInfo from './DriverInfo'; // Importe o componente DriverInfo
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 50,
-    padding: 16,
-    backgroundColor: 'white',
-  },
-  boldText: {
-    fontSize: 30,
-    color: 'red',
-  },
-  text: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 15,
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 15,
   },
   map: {
     flex: 1,
-    width: '100%',
   },
+  actionButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 16,
+  },
+  actionButton: {
+    backgroundColor: 'blue',
+    padding: 12,
+    borderRadius: 8,
+  },
+  actionButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  // ... outros estilos
 });
 
 export default function App() {
-  const [currentLatitude, setCurrentLatitude] = useState('-23.9330843');
-  const [currentLongitude, setCurrentLongitude] = useState('-46.3017265');
-  const [errorMsg, setErrorMsg] = useState('');
-  const [showMap, setShowMap] = useState(false);
-
-  const requestLocationPermission = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status === 'granted') {
-      setShowMap(true);
-    } else {
-      setShowMap(false);
-      Alert.alert('Permissão de Localização', 'Você negou a permissão para acessar a localização em tempo real.');
-    }
-  };
-
-  const getLocation = async () => {
-    let location = await Location.getCurrentPositionAsync({});
-    setCurrentLatitude(location.coords.latitude.toString());
-    setCurrentLongitude(location.coords.longitude.toString());
-  };
+  const [destination, setDestination] = useState(null);
+  const [userLocation, setUserLocation] = useState(null);
+  const [showDriverInfo, setShowDriverInfo] = useState(false);
 
   useEffect(() => {
-    if (showMap) {
-      getLocation(); // Chama a função para obter a localização ao iniciar o mapa
-    }
-  }, [showMap]);
+    // ... código anterior para obter a localização do usuário
+  }, []);
+
+  const handlePress = (event) => {
+    const { latitude, longitude } = event.nativeEvent.coordinate;
+    // Quando o usuário clica no mapa, definimos a localização de destino
+    setDestination({ latitude, longitude });
+  };
+
+  const handleRequestRide = () => {
+    // Quando o usuário solicita a viagem, você pode definir as informações do motorista e custo da viagem
+    // Isso é apenas um exemplo, você deve obter esses dados de onde quer que eles venham
+    const driverName = 'Nome do Motorista';
+    const estimatedCost = 25.00; // Altere para a estimativa real
+
+    setShowDriverInfo(true);
+  };
+
+  const handleClearDestination = () => {
+    setDestination(null);
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.boldText}>Você está Aqui</Text>
-      {showMap && (
-        <>
-          <Text style={styles.text}>Latitude: {currentLatitude}</Text>
-          <Text style={styles.text}>Longitude: {currentLongitude}</Text>
-        </>
-      )}
-      <View style={styles.button}>
-        <Button title="Obter Localização" onPress={requestLocationPermission} />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        onPress={handlePress}
+        initialRegion={{
+          latitude: -23.9330843, // Latitude inicial
+          longitude: -46.3017265, // Longitude inicial
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {/* Marcador para a localização atual */}
+        {userLocation && (
+          <Marker coordinate={userLocation}>
+            {/* ... */}
+          </Marker>
+        )}
+
+        {/* Marcador para o local de destino */}
+        {destination && (
+          <Marker coordinate={destination}>
+            {/* ... */}
+          </Marker>
+        )}
+      </MapView>
+
+      {/* Interface de ação */}
+      <View style={styles.actionButtonContainer}>
+        <TouchableOpacity
+          style={styles.actionButton}
+          onPress={handleClearDestination}
+        >
+          <Text style={styles.actionButtonText}>Limpar Destino</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.actionButton}
+          onPress={handleRequestRide}
+          disabled={!destination} // Desabilita o botão se não houver destino definido
+        >
+          <Text style={styles.actionButtonText}>Solicitar Viagem</Text>
+        </TouchableOpacity>
       </View>
-      {showMap && (
-        <MapView provider={PROVIDER_GOOGLE} style={styles.map} showsUserLocation={true} />
-      )}
-      <StatusBar style="auto" />
+
+      {/* Modal de informações do motorista e custo da viagem */}
+      <Modal
+        visible={showDriverInfo}
+        animationType="slide"
+        transparent={true}
+      >
+        <View style={styles.modalContainer}>
+          <DriverInfo
+            driverName="Nome do Motorista"
+            estimatedCost={25.00} // Altere para a estimativa real
+            onCancel={() => setShowDriverInfo(false)}
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
-
---
+++ DriverInfo.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+const DriverInfo = ({ driverName, estimatedCost, onCancel }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Informações do Motorista</Text>
+      <Text>Nome do Motorista: {driverName}</Text>
+      <Text>Estimativa de Custo: R$ {estimatedCost}</Text>
+      <TouchableOpacity style={styles.cancelButton} onPress={onCancel}>
+        <Text style={styles.cancelButtonText}>Cancelar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  cancelButton: {
+    backgroundColor: 'red',
+    padding: 12,
+    borderRadius: 8,
+    marginTop: 16,
+  },
+  cancelButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+export default DriverInfo;
